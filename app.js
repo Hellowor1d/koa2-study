f158d7ba6f11adb5fe339e0c848da6ad55019577
HelloWor1d <1034597776@qq.com>
2017-04-09 10:33:59 +0800

    原生KOA2实现路由，练习完成主要用到fs.readFile读取view文件夹下的各个页面文件并返回给浏览器

--- app.js
+++ app.js
@@ -3,32 +3,75 @@
 const Koa = require('koa')
 //创建一个 Koa 对象表示 WebApp 本身
 const app = new Koa()
+const fs = require('fs')
 
 
-app.use(async (ctx,next)=>{
-   
+app.use(async(ctx, next) => {
+
     console.log(`request method: ${ctx.request.method} ${ctx.request.url}`)
-     await next()
+    await next()
 })
 
-app.use(async (ctx, next) => {
+app.use(async(ctx, next) => {
     const start = new Date().getTime(); // 当前时间
     await next(); // 调用下一个middleware
     const ms = new Date().getTime() - start; // 耗费时间
     console.log(`Time: ${ms}ms`); // 打印耗费时间
 });
 
-app.use(async (ctx,next)=>{
+app.use(async(ctx, next) => {
     await next()
     // ctx.response.type = 'text/html'
     // ctx.response.body = '<h1>Hello,koa2!</h1>'
 })
+//渲染函数，是为了把view文件夹中的页面信息返回给浏览器
+function render(url) {
+   
+    return new Promise((resolve, reject) => {
+        let path = `./view/${url}`
+        fs.readFile(path, 'utf-8', (err, data) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        })
+    })
 
-app.use(async (ctx,next)=>{
-    ctx.response.body = ctx.request.url
-    console.log(`nihao + ${ctx.body}`)
+}
+
+async  function route(url){
+    let view  = '404.html'
+    switch(url){
+        case '/':
+        view = 'index.html'
+        break
+
+        case '/index':
+        view = 'index.html'
+        break
+
+        case '/todo':
+        view = 'todo.html'
+        break
+
+        case '/404':
+        view = '404.html'
+        break 
+
+        default:
+        break
+
+    }
+    let html = await render(view)
+    return html
+}
+
+app.use( async ( ctx ) => {
+  let url = ctx.request.url
+  let html = await route( url )
+  ctx.body = html
 })
 
 app.listen(3000)
 console.log('app started at port 3000')
-
