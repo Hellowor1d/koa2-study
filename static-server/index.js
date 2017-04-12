4a8ec07c4da0e6ea9d7c8f975199dc1a395a5c71
HelloWor1d <1034597776@qq.com>
2017-04-12 22:52:14 +0800

    static server index.js 文件基本完成，主要包括根据后缀判断请求文件类型，读取内容，返回对应内容

--- static-server/index.js
+++ static-server/index.js
@@ -1,6 +1,33 @@
+const Koa = require('koa')
 const path = require('path')
+const content = require('./util/content')
+const mimes = require('./util/mimes')
 
-console.log(__dirname)
-console.log(__filename)
-console.log(process.cwd())
-console.log(path.resolve('./'))
+const app = new Koa()
+
+const staticPath = './static/'
+
+function parseMime(url){
+    let extName = path.extname(url)
+    extName = extName ? extName.slice(1):'unknow'
+    return mimes[extName]
+}
+app.use(async (ctx)=>{
+    let fullStaticPath = path.join(__dirname,staticPath)
+    let _content = await content(ctx,fullStaticPath)
+    let _mime = parseMime(ctx.url)
+    if(_mime){
+        ctx.type = _mime
+    }
+
+    if(_mime && _mime.indexOf('image/')>=0){
+        ctx.res.writeHead(200)
+        ctx.res.write(_content,'binary')
+        ctx.res.end()
+    }else{
+        ctx.body = _content
+    }
+})
+
+app.listen(3001)
+console.log('static server is starting at port 3001')
