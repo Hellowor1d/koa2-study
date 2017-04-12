76045cc900a6169c3a6a97204985087da75af485
HelloWor1d <1034597776@qq.com>
2017-04-13 00:02:24 +0800

    添加可以遍历文件夹的功能模块

---
+++ static-server/util/walk.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const mimes = require('mimes')
+
+function walk(reqPath) {
+    let files = fs.readdirSync(reqPath)
+
+    let dirList = [],
+        fileList = []
+
+    for (let i = 0, len = files.length; i < len; i++) {
+        let item = files[i]
+        let itemArr = item.split('\.')
+        let itemMime = (itemArr.length > 1) ? itemArr[itemArr.length - 1] : "undefined"
+        if (typeof mimes[itemMime] === "undefined") {
+            dirList.push(files[i]);
+        } else {
+            fileList.push(files[i]);
+        }
+    }
+ let result = dirList.concat( fileList );
+
+  return result;
+
+}
+module.exports = walk;
