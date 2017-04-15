df7cfc60a89d219132b4986da0e2bb39f625f1ec
HelloWor1d <1034597776@qq.com>
2017-04-15 20:07:22 +0800

    koa-session的方式设置session，还是得继续深挖session用法

--- koa-storage/koa-session.js
+++ koa-storage/koa-session.js
@@ -1,46 +1,46 @@
 //第一种设置session的方式：koa-generic-session  和 koa-redis 搭配。参照：https://github.com/koajs/generic-session
 //注意：必须事先开启redis数据库，才能正常
 
-var session = require('koa-generic-session');
-var redisStore = require('koa-redis');
-var koa = require('koa');
+// var session = require('koa-generic-session');
+// var redisStore = require('koa-redis');
+// var koa = require('koa');
 
-var app = new koa();
-app.keys = ['keys', 'keykeys'];
-app.use(session({
-  store: redisStore()
-}));
+// var app = new koa();
+// app.keys = ['keys', 'keykeys'];
+// app.use(session({
+//   store: redisStore()
+// }));
 
-app.use(function *() {
-  switch (this.path) {
-  case '/get':
-    get.call(this);
-    break;
-  case '/remove':
-    remove.call(this);
-    break;
-  case '/regenerate':
-    yield regenerate.call(this);
-    break;
-  }
-});
+// app.use(function *() {
+//   switch (this.path) {
+//   case '/get':
+//     get.call(this);
+//     break;
+//   case '/remove':
+//     remove.call(this);
+//     break;
+//   case '/regenerate':
+//     yield regenerate.call(this);
+//     break;
+//   }
+// });
 
-function get() {
-  var session = this.session;
-  session.count = session.count || 0;
-  session.count++;
-  this.body = session.count;
-}
+// function get() {
+//   var session = this.session;
+//   session.count = session.count || 0;
+//   session.count++;
+//   this.body = session.count;
+// }
 
-function remove() {
-  this.session = null;
-  this.body = 0;
-}
+// function remove() {
+//   this.session = null;
+//   this.body = 0;
+// }
 
-function *regenerate() {
-  get.call(this);
-  yield this.regenerateSession();
-  get.call(this);
-}
+// function *regenerate() {
+//   get.call(this);
+//   yield this.regenerateSession();
+//   get.call(this);
+// }
 
-app.listen(8080);
+// app.listen(8080);
---
+++ koa-storage/koa-session2.js
@@ -0,0 +1,27 @@
+const session = require('koa-session');
+const Koa = require('koa');
+const app = new Koa();
+
+app.keys = ['some secret hurr'];
+
+const CONFIG = {
+  key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+  maxAge: 86400000, /** (number) maxAge in ms (default is 1 days) */
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: true, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+};
+app.use(session(CONFIG, app));
+// or if you prefer all default config, just use => app.use(session(app));
+
+app.use(ctx => {
+  // ignore favicon
+  if (ctx.path === '/favicon.ico') return;
+
+  let n = ctx.session.views || 0;
+  ctx.session.views = ++n;
+  ctx.body = n + ' views'+JSON.stringify(ctx)+`${JSON.stringify(ctx.session)}`;
+});
+
+app.listen(3000);
+console.log('listening on port 3000');
